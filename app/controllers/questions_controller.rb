046dec4e1c9f55a85cd1d0d7771c2d0445679260
Ludovic Villain <chadludo@gmail.com>
2020-11-16 14:37:43 +0100

    stupid coach add css

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,16 +1,17 @@
 class QuestionsController < ApplicationController
-    def ask
-    end
 
-    def answer
-      @question = params[:question]
+  def ask
+  end
 
-      if @question == 'I am going to work'
-        @coach_answer = 'Great!'
-      elsif @question.include?('?')
-        @coach_answer = 'Silly question, get dressed and go to work!'
-      else
-        @coach_answer = "I don't care, get dressed and go to work!"
-      end
+  def answer
+    @question = params[:question]
+
+    if @question == 'I am going to work'
+      @coach_answer = 'Great!'
+    elsif @question.include?('?')
+      @coach_answer = 'Silly question, get dressed and go to work!'
+    else
+      @coach_answer = "I don't care, get dressed and go to work!"
     end
+  end
 end
